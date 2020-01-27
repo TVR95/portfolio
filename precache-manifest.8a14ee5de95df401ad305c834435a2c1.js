5d36b4fd830a2e6efdb66ae5849016001602a9d6
TVR95 <kamil.szczepanski1@gmail.com>
2020-01-27 15:27:19 +0100

    Updates

---
+++ precache-manifest.8a14ee5de95df401ad305c834435a2c1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab2144b08a24fd07e440e7d51175efe5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e85920d673a8b198447d",
+    "url": "/portfolio/static/css/main.a65193d1.chunk.css"
+  },
+  {
+    "revision": "c5d4b727d35e5a59ba59",
+    "url": "/portfolio/static/js/2.33a145c7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.33a145c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e85920d673a8b198447d",
+    "url": "/portfolio/static/js/main.34788dbc.chunk.js"
+  },
+  {
+    "revision": "6fef3673099b4dcdb973",
+    "url": "/portfolio/static/js/runtime-main.fed7eca6.js"
+  },
+  {
+    "revision": "6eca577426d5fece6c0a1fa7f965a652",
+    "url": "/portfolio/static/media/Home-c.6eca5774.png"
+  },
+  {
+    "revision": "4a93a56bf654d8f6d77b9920881b3196",
+    "url": "/portfolio/static/media/Home.4a93a56b.png"
+  },
+  {
+    "revision": "3d40302b0864db8e2210e295152d8543",
+    "url": "/portfolio/static/media/MongoDB_Logo_FullColorBlack_RGB.3d40302b.png"
+  },
+  {
+    "revision": "5c69e565f1ae6d7717fb3ecd2be004ad",
+    "url": "/portfolio/static/media/angular.5c69e565.svg"
+  },
+  {
+    "revision": "f80a91ba03fbf0e99ea6949aa3a6399e",
+    "url": "/portfolio/static/media/mysql.f80a91ba.svg"
+  },
+  {
+    "revision": "46f758661588703b190fb4d2aae3847a",
+    "url": "/portfolio/static/media/nodejs-new-pantone-black.46f75866.png"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/portfolio/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "229ed79927c3992dc1d7a00343cf9952",
+    "url": "/portfolio/static/media/spring-by-pivotal.229ed799.png"
+  }
+]);
